28e250d229a9c8bbb1550eecbd0eb95ff2367007
Phil Nelson <phil@extrafuture.com>
2020-05-02 10:57:27 -0700

    Add saving blank data to files with no ID3 tags. Add artist checking to ID3 checker. Add directory arg to checker.

--- check_mp3_titles.py
+++ check_mp3_titles.py
@@ -1,20 +1,46 @@
 from os import walk
 from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3NoHeaderError, ID3
+from mutagen.mp3 import MP3
+import argparse
 
-prefix = "/home/phil/oldtimeradio/audio_sources/Dimension_X"
+parser = argparse.ArgumentParser()
+parser.add_argument("directory",help="Full path to directory with mp3s")
+args = parser.parse_args()
+
+audio_directory = args.directory
 
 f = []
-for (dirpath, dirnames, filenames) in walk(prefix):
+total_missing = 0
+
+for (dirpath, dirnames, filenames) in walk(audio_directory):
     f.extend(filenames)
     break
 
 for file in f:
-    audio = EasyID3("{}/{}".format(prefix,file))
-    
-    title = audio.get('title', None)
+    if file.lower().endswith('.mp3'):
+        try:
+            audio = EasyID3("{}/{}".format(audio_directory,file))
+        
+            title = audio.get('title', None)
+            artist = audio.get('artist', None)
 
-    if title is None:
-        print(file)
-        break
+            if title is None:
+                print("{} has no title attribute.".format(file))
 
-    #audio.save()
+            if artist is None:
+                print("{} has no artist attribute.".format(file))
+                
+            if title or artist is None:
+                total_missing+=1
+
+        except ID3NoHeaderError:
+            print("{} has no ID3 tags. Saving blank ones to it.".format(file))
+            audio = MP3("{}/{}".format(audio_directory,file))
+            audio.add_tags()
+            
+            # Save blank ID3 tags to broken file
+            audio.save("{}/{}".format(audio_directory,file))
+            break
+
+print("{} files missing ID3 tags".format(total_missing))
