385bc211c0b4c0d897b0fdca7e3e3920e0c55dc2
Phil Nelson <phil@extrafuture.com>
2020-05-10 15:02:10 -0700

    Playlist now includes files with capital M cause I'm lazy. TODO: Fix twice, just str.lower it.

--- generate_playlist_from_directory.sh
+++ generate_playlist_from_directory.sh
@@ -1,1 +1,2 @@
 playlist=${2} ; if [ -f $playlist ]; then rm $playlist ; fi ; for f in ${1}*.mp3; do echo "$f" >> "$playlist"; done
+playlist=${2} ; for f in ${1}*.Mp3; do echo "$f" >> "$playlist"; done
